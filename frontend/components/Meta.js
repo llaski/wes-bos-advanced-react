3a487f9ae68e8c11fa0f7c1b1210c27db3f56d3c
Larry Laski <larry.laski@gmail.com>
2019-10-08 21:57:23 -0400

    frontend lessons

---
+++ frontend/components/Meta.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import React from "react";
+
+const Meta = () => (
+<Head>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta charset="utf-8" />
+    <link rel="shortcut icon" href="/static/favicon.png" />
+    <link rel="stylesheet" href="/static/nprogress.css" />
+    <title>Sick Fits!</title>
+</Head>
+)
+
+export default Meta;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Link from 'next/link'
 
 const Home = () => {
   return (
     <>
-      <p>Heyooo</p>
-      <Link href="/sell">
-        <a>Sell</a>
-      </Link>
+      <p>Home</p>
     </>
   );
 };
