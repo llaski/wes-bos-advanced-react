fd56f9471b5bd024f454785a3193384896be8699
Larry Laski <larry.laski@gmail.com>
2019-10-10 22:28:52 -0400

    General item setup

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutation = {};
+const Mutation = {
+    async createItem(parent, args, context, info) {
+        //@todo check if they are logged in
+        const item = await context.db.mutation.createItem(
+            {
+                data: {
+                    ...args,
+                },
+            },
+            info,
+        )
 
-module.exports = Mutation;
+        return item
+    },
+}
+
+module.exports = Mutation
